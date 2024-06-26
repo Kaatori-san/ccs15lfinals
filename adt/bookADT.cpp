4be8090993cc819e4660f44e918a6ea828214a6d
kaatori-san <kurtdy024@gmail.com>
2024-06-26 17:56:21 +0800

    update

---
+++ adt/bookADT.cpp
@@ -0,0 +1,21 @@
+class bookADT
+{
+private:
+    int bookID;
+    int bookTitle;
+    int bookGenre;
+    int bookProduction;
+    int bookNumCopies;
+    int bookIMGFileName;
+public:
+    bookADT(/* args */);
+    ~bookADT();
+};
+
+bookADT::bookADT(/* args */)
+{
+}
+
+bookADT::~bookADT()
+{
+}
--- main.cpp
+++ main.cpp
@@ -1,51 +1,51 @@
 #include<iostream>
-#include"./adt/videoADT.cpp"
+#include"./adt/bookADT.cpp"
 #include"./adt/customerADT.cpp"
 using namespace std;
 
 main(){
     int choice;
-    cout<<"[1] New Video"<<endl;
-    cout<<"[2] Rent a Video"<<endl;
-    cout<<"[3] Return a Video"<<endl;
-    cout<<"[4] Show Video Details"<<endl;
-    cout<<"[5] Display all Videos"<<endl;
-    cout<<"[6] Check Video Availability"<<endl;
+    cout<<"[1] New Book"<<endl;
+    cout<<"[2] Rent a Book"<<endl;
+    cout<<"[3] Return a Book"<<endl;
+    cout<<"[4] Show Book Details"<<endl;
+    cout<<"[5] Display all Books"<<endl;
+    cout<<"[6] Check Book Availability"<<endl;
     cout<<"[7] Customer Maintenance"<<endl;
     cout<<"[8] Exit Program"<<endl;
     cout<<"Enter your choice: ";cin>>choice;
     switch (choice){
 
         case 1:{
-            cout<<"[1] New Video"<<endl;
+            cout<<"New Book"<<endl;
             break;
         }
         case 2:{
-            cout<<"[2] Rent a Video"<<endl;
+            cout<<"Rent a Book"<<endl;
             break;
         }
         case 3:{
-            cout<<"[3] Return a Video"<<endl;
+            cout<<"Return a Book"<<endl;
             break;
         }
         case 4:{
-            cout<<"[4] Show Video Details"<<endl;
+            cout<<"Show Book Details"<<endl;
             break;
         }
         case 5:{
-            cout<<"[5] Display all Videos"<<endl;
+            cout<<"Display all Books"<<endl;
             break;
         }
         case 6:{
-            cout<<"[6] Check Video Availability"<<endl;
+            cout<<"Check Book Availability"<<endl;
             break;
         }
         case 7:{
-            cout<<"[7] Customer Maintenance"<<endl;
+            cout<<"Customer Maintenance"<<endl;
             break;
         }
         case 8:{
-            cout<<"[8] Exit Program"<<endl;
+            cout<<"Exit Program"<<endl;
             break;
         }
         default:{
