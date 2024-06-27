f511b1b5844e535848ed99920fe68d4abcc64956
moodyashit <156320712+moodyashit@users.noreply.github.com>
2024-06-27 18:28:20 +0800

    Update main.cpp
    updated omsm

--- main.cpp
+++ main.cpp
@@ -1,59 +1,92 @@
-#include<iostream>
-#include"./adt/bookADT.h"
-#include"./adt/customerADT.h"
+#include <iostream>
+#include "adt/bookADT.h"
+#include "adt/customerADT.h"
+#include "adt/customerRentADT.h"
 using namespace std;
 
-main(){
+int main() {
     int choice;
-    cout<<"[1] New Book"<<endl;
-    cout<<"[2] Rent a Book"<<endl;
-    cout<<"[3] Return a Book"<<endl;
-    cout<<"[4] Show Book Details"<<endl;
-    cout<<"[5] Display all Books"<<endl;
-    cout<<"[6] Check Book Availability"<<endl;
-    cout<<"[7] Customer Maintenance"<<endl;
-    cout<<"[8] Exit Program"<<endl;
-    cout<<"Enter your choice: ";cin>>choice;
-    bookADT myBook;
-    do
-    {
-        switch (choice){
+    BookADT myBook;
+    CustomerADT myCustomer;
+    CustomerRentADT myRental;
 
-        case 1:{
-            myBook.newBook();
-            break;
+    do {
+        cout << "[1] New Book" << endl;
+        cout << "[2] Rent a Book" << endl;
+        cout << "[3] Return a Book" << endl;
+        cout << "[4] Show Book Details" << endl;
+        cout << "[5] Display all Books" << endl;
+        cout << "[6] Check Book Availability" << endl;
+        cout << "[7] Customer Maintenance" << endl;
+        cout << "[8] Exit Program" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                myBook.newBook();
+                break;
+            }
+            case 2: {
+                myRental.rentBook(myBook);  
+                break;
+            }
+            case 3: {
+                myRental.returnBook(myBook);  
+                break;
+            }
+            case 4: {
+                myBook.showBookDetails();
+                break;
+            }
+            case 5: {
+                myBook.displayAllBooks();
+                break;
+            }
+            case 6: {
+                cout << "Check Book Availability\nBook Title: ";
+                cin.ignore();
+                string title;
+                getline(cin, title);
+                if (myBook.checkBookAvailability(title)) {
+                    cout << "Book Available!" << endl;
+                } else {
+                    cout << "Book Not Available!" << endl;
+                }
+                break;
+            }
+            case 7: {
+                cout << "[1] Add Customer" << endl;
+                cout << "[2] Show Customer Details" << endl;
+                cout << "[3] Print All Customers" << endl;
+                cout << "Enter your choice: ";
+                int subChoice;
+                cin >> subChoice;
+                switch (subChoice) {
+                    case 1: {
+                        myCustomer.addCustomer();
+                        break;
+                    }
+                    case 2: {
+                        myCustomer.showCustomerDetails();
+                        break;
+                    }
+                    case 3: {
+                        myCustomer.printAllCustomers();
+                        break;
+                    }
+                    default:
+                        cout << "Invalid input. Please try again." << endl;
+                }
+                break;
+            }
+            case 8:
+                cout << "Exit Program" << endl;
+                break;
+            default:
+                cout << "Invalid input. Please try again." << endl;
         }
-        case 2:{
-            cout<<"Rent a Book"<<endl;
-            break;
-        }
-        case 3:{
-            cout<<"Return a Book"<<endl;
-            break;
-        }
-        case 4:{
-            cout<<"Show Book Details"<<endl;
-            break;
-        }
-        case 5:{
-            cout<<"Display all Books"<<endl;
-            break;
-        }
-        case 6:{
-            cout<<"Check Book Availability"<<endl;
-            break;
-        }
-        case 7:{
-            cout<<"Customer Maintenance"<<endl;
-            break;
-        }
-        case 8:{
-            cout<<"Exit Program"<<endl;
-            break;
-        }
-        default:{
-            cout<<"Invalid input. Please try again."<<endl;
-        }
-    }
-    } while (choice!=8);
+    } while (choice != 8);
+
+    return 0;
 }
