d6d6a55dfc2aa3d14a1babe4a9513e1cc61518b2
moodyashit <156320712+moodyashit@users.noreply.github.com>
2024-07-04 21:25:48 +0800

    I forgot what I did here

--- adt/customerADT.h
+++ adt/customerADT.h
@@ -1,80 +1,67 @@
-// customerADT
 
 #ifndef CUSTOMERADT_H
 #define CUSTOMERADT_H
 
-#include <iostream>  // Handling input and output operations in the console log
-#include <fstream>  // used for reading and writing files. 
-#include <string>   // used for handling string data, such as storing book details etc 
-#include <vector>   // used to store nad manage the collections of Book in a resizable array-like structure
-#include "bookADT.h"  // connects to other ADT files
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "bookADT.h"
 #include "customerRentADT.h"
 
 #ifdef _WIN32
-#include <direct.h> // for windows systems
+#include <direct.h>
 #else
-#include <sys/stat.h>  // for other systems
+#include <sys/stat.h>
 #endif
 
 using namespace std;
 
 class CustomerADT {
 public:
-    // add a new customer
     void addCustomer();
 
-    // shows specific details of a customer by their ID
     void showCustomerDetails();
 
-    // Prints every details of all customers in the customers.txt file
     void printAllCustomers();
 
 private:
-    // Structure defining a Customer
     struct Customer {
         int id;
         string name;
         string address;
     };
 
-    const char* customerPath = "./data/customers.txt";  // Path to the customer data file
-    vector<Customer> customers;  // Vector to store customer records
+    const char* customerPath = "./data/customers.txt";
+    vector<Customer> customers;
 
-    // Function to get the next available customer ID
     int getNextID();
 
-    // Function to load customers from file into memory
     bool loadCustomers();
 
-    // Function to save customers from memory to file
     void saveCustomers();
 
-    // Function to append a new customer record to the file
     void appendCustomer(const Customer& customer);
 
-    // Helper function to print error messages
     void printError(const string& msg) {
         cerr << "Error: " << msg << endl;
     }
 
-    // Helper function to wait for user input
     void waitForUserInput() {
         cout << "Press enter to continue...";
         cin.ignore();
     }
 };
 
-//  getNextID function
 int CustomerADT::getNextID() {
     return customers.empty() ? 1 : customers.back().id + 1;
 }
 
-//  loadCustomers function
 bool CustomerADT::loadCustomers() {
     ifstream file(customerPath);
     if (!file) {
         printError("Failed to open customers file.");
-        return false;  // Return false if file cannot be opened
+        return false;
     }
 
     customers.clear();
@@ -92,10 +79,9 @@ bool CustomerADT::loadCustomers() {
         }
     }
     file.close();
-    return true;  // Return true if customers are loaded successfully
+    return true;
 }
 
-//  appendCustomer function - responsible for putting new customer data in the customers.txt
 void CustomerADT::appendCustomer(const Customer& customer) {
 #ifdef _WIN32
     _mkdir("./data");
@@ -103,13 +89,13 @@ void CustomerADT::appendCustomer(const Customer& customer) {
     mkdir("./data", 0777);
 #endif
 
-    ofstream file(customerPath, ios::app);  // if customer.txt file cannot be opened this is the error message
+    ofstream file(customerPath, ios::app);
     if (!file) {
         printError("Unable to open file for writing.");
         return;
     }
 
-    file << "Customer ID: " << customer.id << '\n'    // if file is succesfully read, this shows
+    file << "Customer ID: " << customer.id << '\n'
          << "Name: " << customer.name << '\n'
          << "Address: " << customer.address << '\n'
          << "---\n";
@@ -117,13 +103,11 @@ void CustomerADT::appendCustomer(const Customer& customer) {
     file.close();
 }
 
-// addCustomer function
 void CustomerADT::addCustomer() {
     if (!loadCustomers()) {
-        return;  // Exit if loading customers fails
+        return;
     }
 
-    // Customer Details
     Customer customer;
     customer.id = getNextID();
     cout << "Add Customer\nName: ";
@@ -135,13 +119,12 @@ void CustomerADT::addCustomer() {
     customers.push_back(customer);
     appendCustomer(customer);
     cout << "New Customer Added!" << endl;
-    waitForUserInput();   // waits for the user to click on the "enter" key.
+    waitForUserInput();
 }
 
-// I showCustomerDetails function
 void CustomerADT::showCustomerDetails() {
     if (!loadCustomers()) {
-        return;  // Exit if loading customers fails
+        return;
     }
 
     int id;
@@ -149,52 +132,49 @@ void CustomerADT::showCustomerDetails() {
     cin >> id;
     cin.ignore();
 
-    bool found = false;  // tracked whether the user entered an ID that's inside the file
-    for (const auto& customer : customers) {  // repeats over each customer in the customers vector
-        if (customer.id == id) {   // checks the  customer's ID
-            cout << "ID: " << customer.id << "\nName: " << customer.name << "\nAddress: " << customer.address << endl;  // Prints the customer details
-            found = true;  // true is to indicate that the ID was found inside the file
+    bool found = false;
+    for (const auto& customer : customers) {
+        if (customer.id == id) {
+            cout << "\n"<< "ID: " << customer.id << "\nName: " << customer.name << "\nAddress: " << customer.address << endl;
+            found = true;
             break;
         }
     }
 
-    if (!found) { // checks if the found variable is false. if It's true then the customer ID was found and this section will be skipped.
-    // if not, this section will be executed
+    if (!found) {
         cout << "Customer ID Not Found!" << endl;
     }
     waitForUserInput();
 }
 
-// printAllCustomers function
 void CustomerADT::printAllCustomers() {
     if (!loadCustomers()) {
-        return;  // Exit if loading customers fails
+        return;
     }
 
-    if (customers.empty()) {  // Checks if the 'customers' vector is empty, if it's empty then this print will be shown. 
+    if (customers.empty()) {
         cout << "No customers found." << endl;
         return;
     }
 
-    for (const auto& customer : customers) {  // This loop repeats itself over each cusyomer in the 'customers' vector
-        cout << "ID: " << customer.id << "\nName: " << customer.name << "\nAddress: " << customer.address << endl;
+    for (const auto& customer : customers) {
+        cout << "ID: " << customer.id << "\nName: " << customer.name << "\nAddress: " << customer.address << "\n-------" << endl;
     }
     waitForUserInput();
-    cin.ignore();  // ignores any remaining characters in the input buffer. Ensures that the input operations will work correctly
+    cin.ignore();
 }
 
-// Implementation of saveCustomers function
 void CustomerADT::saveCustomers() {
-#ifdef _WIN32   // directory for windows system
+#ifdef _WIN32
     _mkdir("./data");
 #else
-    mkdir("./data", 0777);    // other operating systems
+    mkdir("./data", 0777);
 #endif
 
-    ofstream file(customerPath);  // write to the file that directs to customers.txt
-    if (!file) {  // checks if the file stream was successfully opened.
-        printError("Unable to open file for writing.");  // if the file could not be opened this prints
-        return;  // exits the saveCustomers function
+    ofstream file(customerPath);
+    if (!file) {
+        printError("Unable to open file for writing.");
+        return;
     }
 
     for (const auto& customer : customers) {
